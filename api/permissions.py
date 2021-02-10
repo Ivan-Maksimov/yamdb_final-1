506223d324fb3205266af75ca8b552ee77aef7b5
Sergey Medvedev <medvedevsm1989@yandex.ru>
2021-02-11 00:38:16 +0300

    upd: 33

--- api/permissions.py
+++ api/permissions.py
@@ -39,6 +39,6 @@ class IsAuthorOrModeratorOrAdminOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or (
-            (request.user == obj.author) or (
-                request.user.role in ('admin', 'moderator'))
+                (request.user == obj.author) or (
+                 request.user.role in ('admin', 'moderator'))
         )
